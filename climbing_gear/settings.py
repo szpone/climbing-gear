083e82a323f4f6e6b9818e71a43203e22ad348f3
szpone <nikola.adamus@gmail.com>
2019-05-05 21:04:20 +0200

    add db conf and pre-commit conf

--- climbing_gear/settings.py
+++ climbing_gear/settings.py
@@ -77,9 +77,12 @@ WSGI_APPLICATION = "climbing_gear.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'climbinggear',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
