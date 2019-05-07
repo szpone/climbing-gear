300f60650b101750233f4068549d85096fbe295b
szpone <nikola.adamus@gmail.com>
2019-05-07 19:58:24 +0200

    UPD: refactor models - change names and relations

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-from .models import User
+Climber = get_user_model()
 
 # Register your models here.
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+@admin.register(Climber)
+class ClimberAdmin(admin.ModelAdmin):
     list_display = ("username", "first_name", "last_name", "email")
--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 
-class User(AbstractUser):
+class Climber(AbstractUser):
     username = models.CharField(max_length=64, unique=True)
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=64)
