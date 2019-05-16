14f2cf168d4a7fd1bb97ca8c991e9b6383ace491
szpone <nikola.adamus@gmail.com>
2019-05-16 21:19:04 +0200

    add user test

--- users/tests/test_climber_schema.py
+++ users/tests/test_climber_schema.py
@@ -37,3 +37,18 @@ def test_get_climbers(graph_client):
     executed = graph_client.execute(query)
 
     assert len(executed["data"]["climbers"]) == Climber.objects.all().count()
+
+
+@pytest.mark.django_db
+def test_get_none_climber(graph_client):
+    query = """
+            query {
+                climber(id: 99999) {
+                    id
+                }
+            }
+        """
+
+    executed = graph_client.execute(query)
+
+    assert executed["data"]["climber"] is None
